4c897c78fae3dd16d97f249c4a92f0affa2e5c47
Avi Vahl <avi.vahl@wix.com>
2019-03-31 04:13:39 +0300

    Deps: typescript@3.4.1, tslint@5.14.0 - regenerated yarn.lock - fixed ReduceTuppleOn to be compatible with new ts.

--- lib/tupple-utils.d.ts
+++ lib/tupple-utils.d.ts
@@ -1,8 +1,9 @@
-
 export type TuppleToUnion<T> = T extends Array<infer ITEMS> ? ITEMS : never
 
-export type ReduceTuppleOn<T extends any[], FIELD, EXTRACTFIELD> = FIELD extends keyof TuppleToUnion<T> ?
-    EXTRACTFIELD extends keyof TuppleToUnion<T> ?
-    {
-        [key in TuppleToUnion<T>[FIELD]]: Extract<TuppleToUnion<T>, { [exc in FIELD]: key }>[EXTRACTFIELD]
-    } : never : never
+export type ReduceTuppleOn<
+    T extends any[],
+    FIELD extends keyof TuppleToUnion<T>,
+    EXTRACTFIELD extends keyof TuppleToUnion<T>
+> = {
+    [key in TuppleToUnion<T>[FIELD]]: Extract<TuppleToUnion<T>, { [exc in FIELD]: key }>[EXTRACTFIELD]
+}
